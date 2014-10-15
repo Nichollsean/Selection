1ba30cb88683bc5ee1cdeeffd1b63ac994dccc4b
Nichollsean <40668@longroad.ac.uk>
2014-10-15 21:06:30 +0100

    Work from home

---
+++ Iteration DEV EX2.py
@@ -0,0 +1,11 @@
+#Sean Nicholls
+#14/10/14
+#Iteration DEV EX2
+
+number=int(input("How many stars would you like to display: "))
+printamount = (number*("*"))
+lines=int(input("How many lines of stars would you like to display: "))
+for number in range(lines):
+    print(printamount)
+
+    
